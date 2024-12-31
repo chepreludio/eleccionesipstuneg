74b258c42cdfd189c050f1f241a65c5d36fce5d8
cesarbellorin <chepreludio@gmail.com>
2024-12-31 18:24:51 -0300

    31122024_2

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,8 @@ import duckdb
 varTitulo = 'Elecciones Ipstuneg 2025'
 varCedula = ''
 varFicha = ''
+cursor = duckdb.connect()
+cursor.execute("""CREATE TABLE tmp AS SELECT * FROM read_csv('db/db.csv');""")
 
 
 st.set_page_config(
@@ -13,71 +15,79 @@ st.set_page_config(
     page_title=varTitulo,    
 )
 
-
-
-#df = pd.read_csv('db/db.csv', sep=";")
-cursor = duckdb.connect()
-cursor.execute("""CREATE TABLE tmp AS SELECT * FROM read_csv('db/db.csv');""")
-st.session_state["authenticated"] = False
-st.session_state["username"] = ''
+#@st.cache_data
+#def load_data():
+#    #df = pd.read_csv('db/db.csv', sep=";")
+#    cursor = duckdb.connect()
+#    cursor.execute("""CREATE TABLE tmp AS SELECT * FROM read_csv('db/db.csv');""")
+#    st.session_state["authenticated"] = False
+#    st.session_state["username"] = ''
+#    return cursor
 
 def getData():
     with st.form(varTitulo):
-        varCedula = st.text_input("Cédula", placeholder="Ingrese su Cédula")
+        varCedula = st.text_input("Cédula", placeholder="Ingrese su Cédula", max_chars=9)
         #varDataNacimiento = st.date_input("Fecha de Nacimiento", min_value=datetime(1934,1,1))
-        varFicha = st.text_input("N Ficha", placeholder="Ingresu su número de ficha")
+        varFicha = st.text_input("N Ficha", placeholder="Ingresu su número de ficha", max_chars=5)
         varEnviado = st.form_submit_button("Ingresar")
 
     return varEnviado
 
 def votar():
-    st.success(st.session_state["username"])
+    st.success(f"Puede iniciar su proceso de elección {st.session_state["username"]}")
     
-    #if varEnviado:
-    #    #if varCedula == "" and varDataNacimiento == datetime.now().date():
-    #    if varCedula == "" and varFicha == "":
-    #        st.warning("Debe Ingresar su Cédula y su Ficha!")
-    #    else:
-    #        #V09950668	07088
-    #        df = cursor.execute(f"SELECT * FROM tmp WHERE CEDULA='{varCedula}' AND FICHA='{varFicha}';").fetchone()
-    #        #st.success(f"{df}")
-    #        if len(df) > 0:
-    #            st.session_state["authenticated"] = True
-    #            st.session_state["username"] = str(df[3])
 
+
+def check_password():
+    """Returns True if the user had a correct password."""
+
+    def login_form():
+        """Form with widgets to collect user information"""
+        with st.form("Elector"):
+            st.text_input("Cédula", placeholder="Ingrese su Cédula", max_chars=9, key="username")
+            st.text_input("N Ficha", placeholder="Ingresu su número de ficha", max_chars=5, type="password", key="password")
+            st.form_submit_button("Ingresar", on_click=password_entered)
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+       
+        if st.session_state["username"] != "" and st.session_state["password"] != "":            
+        
+            df = cursor.execute(f"SELECT * FROM tmp WHERE CEDULA='{st.session_state["username"].upper()}' AND FICHA='{st.session_state["password"]}';").df()
+
+            if len(df) > 0:                
+                st.session_state["username"] = str(df['NOMBRE'].iloc[0])
+                st.session_state["password_correct"] = True                
+                #del st.session_state["password"]  # Don't store the username or password.
+                #del st.session_state["username"]         
+            else:
+                st.session_state["password_correct"] = False
+        else:
+            st.session_state["empty_values"] = True
+            
+    
+    if st.session_state.get("password_correct", False):
+        return True
+    
+    
+    
+    login_form()
+    if "empty_values" in st.session_state:
+        st.warning("Debe Ingresar su Cédula y su Ficha!")
+        del  st.session_state["empty_values"]
+
+    if "password_correct" in st.session_state:
+        st.error("😕 Cédula o Número de Ficha incorrectos")
+    return False
 
 def main():
-    st.title("Elecciones Ipstuneg")    
+    st.title(varTitulo)
 
-    enviado = getData()
-    st.write(enviado)
-    
-    if enviado:
-        #if varCedula == "" and varDataNacimiento == datetime.now().date():
-        st.write(f"{varCedula}")
-        if varCedula == "" and varFicha == "":
-            st.warning("Debe Ingresar su Cédula y su Ficha!")
-        else:
-            #V09950668	07088
-            df = cursor.execute(f"SELECT * FROM tmp WHERE CEDULA='{varCedula}' AND FICHA='{varFicha}';").fetchone()
-            #st.success(f"{df}")
-            if len(df) > 0:
-                st.session_state["authenticated"] = True
-                st.session_state["username"] = str(df[3])
+    if not check_password():
+        st.stop()
     else:
-        st.write(enviado)
-    #
-    #if st.session_state["authenticated"]:
-    #    if st.session_state["username"]:
-    #        st.success(f"Welcome {st.session_state['username']}")
-    #        ...
-    #
-    #    else:
-    #        st.success("Welcome guest")
-    #        ...
-    #else:
-    #    login()
-    #    st.error("Not authenticated")
+        votar()
+    
 
 if __name__ == '__main__':
     main()
